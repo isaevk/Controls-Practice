7f08548ec593010b29048a8cc7328e433d8851f1
Kirill Isaev <thisiskir.dev@ya.ru>
2022-07-27 18:50:42 +0300

    Initial Commit

---
+++ Controls Practice/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Controls Practice
+//
+//  Created by Kirill Dev on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet var switches: [UISwitch]!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonPressed() {
+    }
+    
+    @IBAction func switchToggle(_ sender: UISwitch) {
+    }
+    
+    @IBAction func sliderMoved() {
+    }
+    
+
+    @IBAction func textFiedEdits() {
+    }
+    
+}
+
